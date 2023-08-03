bdcce57e0d0cd419f68d589fb57fecef240b25b1
Naishal Doshi <naishal036@gmail.com>
2023-08-03 11:25:07 +0530

    Add files via upload

---
+++ otp.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$otp=random_int(10000,99999);
+$to_email = "21bmiit096@gmail.com";
+$subject = "OTP VERIFICATION";
+$body = "your otp is $otp";
+$headers = "From: dashtaxigg@gmail.com";
+
+if (mail($to_email, $subject, $body, $headers)) {
+    
+echo "success";
+} else {
+    echo "Email sending failed...";
+}
